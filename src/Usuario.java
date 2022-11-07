949a1a6fc2db1e5fdb1d464df5c8f837e48629dd
Gustavo Rocha <sazaki.dc@hotmail.com>
2022-11-07 08:22:28 -0300

    inclusao readme

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Usuario {
+public class Usuario {
+
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume atual: "+ smartTv.volume);
+
+
+        smartTv.mudarCanal(56);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+
+
+
+    }
 }
